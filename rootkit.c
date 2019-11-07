f091ccc3a3865f65f5d157bd45ff63a9bf18e026
bfoley12 <brendanfoley@optonline.net>
2019-11-07 17:24:32 -0500

    Can now get Kernel Version
    Added 1 functional method: get_kernel_version. It pulls the kernel version to access the proper file in order to start the process of getting hooks into the sys_call_table (in progress, not functional).

--- rootkit.c
+++ rootkit.c
@@ -1,54 +1,99 @@
-/* rootkit..c */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/init.h>
-#include <stdlib.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <asm/uaccess.h>
+//#include <string.h>
 #include "rootkit_conf.conf.h"
 
 MODULE_LICENSE("GPL") ;
 MODULE_AUTHOR("Brendan<brendanfoley1214@gmail.com>") ;
-MODULE_DESCRIPTION("Rootkit for CSE331") ;
-MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("Rootkit Testfile for CSE331") ;
+MODULE_VERSION("0.3");
 
-#define PROC_VERSION "/path/version"
-#define PATH "/boot/System.map-"
+#define PROC_VERSION "/proc/version"
+#define BOOT_PATH "/boot/System.map-"
+/*
+char *get_sys_call_table(char *path){
+	struct file *f;
+	mm_segment_t old_fs;
+	char buff[256];
+	char *kern_ver = kmalloc(256, GFP_KERNEL);
+
+	memset(buff, 0, 256);
+
+	f = filp_open(PROC_VERSION, O_RDONLY, 0);
+	// Error handling failing... Program crashes on above line.
+	/*if(f == NULL){
+		printk(KERN_WARNING "Unable to open file: %s\n", PROC_VERSION);
+		return;
+	}*/
 
 /*
- * Elevates the process euid/uid to root
- * **Will need to give a pointer to the process as an argument (not sure how to do that yet though)
- */
-static void elevate_process(void){
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
 
-}
+	f->f_op->read(f, buff, 256, &f->f_pos);
 
 
-/*
- * Lowers the process euid/uid to its original
- * **Will need to give a pointer to the process as an argument (not sure how to do that yet though)
- */
-static void lower_proccess(void){
+	set_fs(old_fs);
+	return 1;
+}*/
 
-}
+char *get_kernel_version(void){
+	struct file *f;
+	mm_segment_t old_fs;
+	char buff[256];
+	char *kern_ver = kmalloc(256, GFP_KERNEL);
 
-/*
- * Gets the kernel version to be appeneded onto the PATH for accessing SysCallTable
- */
-static char *get_kernel_version(void){
-	char *buff = malloc(sizeof(char) * 20);
+	int i;
+	memset(buff, 0, 256);
+
+	f = filp_open(PROC_VERSION, O_RDONLY, 0);
+	// Error handling failing... Program crashes on above line.
+	/*if(f == NULL){
+		printk(KERN_WARNING "Unable to open file: %s\n", PROC_VERSION);
+		return;
+	}*/
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+
+	f->f_op->read(f, buff, 256, &f->f_pos);
 	
+	i = 14;
+	while(i < 256 && buff[i] != ' '){
+		kern_ver[i-14] = buff[i];
+		i++;
+	}
+	kern_ver[i] = '\0';
+	set_fs(old_fs);
+	return kern_ver;
 }
+
 
 // Loads the LKM
 static int __init rootkit_init(void){
-	printk(KERN_INFO "Rootkit Loaded\n");
+	printk(KERN_INFO "Hello Kernel!");
+	char *kernel_version = get_kernel_version();
+	char *sys_call_address;
+	if(kernel_version == NULL){
+		return 1;
+	}
+	char path[40] = BOOT_PATH;
+	strcat(path, kernel_version);
+	printk(KERN_INFO "Full Boot path is: %s\n", path);
+	//*sys_call_address = find_sys_call_table(*path);
+	return 0;
 }
 
 
 // Exits the LKM
-static int __exit rootkit_exit(void){
+static void __exit rootkit_exit(void){
 	printk(KERN_INFO "Rootkit Unloaded\n");
 }
+
 
 module_init(rootkit_init);
 module_exit(rootkit_exit);
