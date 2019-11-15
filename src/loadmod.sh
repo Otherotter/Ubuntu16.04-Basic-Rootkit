fefa9aeaa00670b983c40c3d44e4a001395bf4c2
bfoley12 <brendanfoley@optonline.net>
2019-11-14 19:24:16 -0500

    Now shows support for multiple object loads
    messed up original upload destination

--- src/loadmod.sh
+++ src/loadmod.sh
@@ -4,8 +4,13 @@ RMMODULE=rootkit
 INSMODULE=rootkit.ko
 
 sudo rmmod $RMMODULE
+#sudo rmmod quick
 make
 sleep 1
 sudo insmod $INSMODULE
+sudo insmod quick.ko
+sleep 1
+sudo rmmod $RMMODULE
+#sudo rmmod quick
 sleep 1
 tail /var/log/kern.log
